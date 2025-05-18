7db5dced55c4ea38cd65769d0805c2ab6ab34209
imyoubin <imyoubin@hanmail.net>
2025-05-18 22:44:20 +0900

    2025.05.18

--- src/com/javaex/ex01/Ractangle.java
+++ src/com/javaex/ex01/Ractangle.java
@@ -11,13 +11,13 @@ public class Ractangle extends Shape{
 		this.height=height;
 	}
 	
-	public void showInfo(){
-		System.out.println("[사각형]#면색:" +super.fillColor+ " "
-				+ " #가로:" + width 
-				+" #세로:" + height + "그렸습니다.");                                                                        
+	public int getWidth() {
+		return width;
 	}
-		
 
+	public void draw(){
+		System.out.println("[사각형]#면색:" +super.fillColor+ " #가로:" + width +" #세로:" + height + " 그렸습니다.");                                                                        
+	}
 }
 
 
